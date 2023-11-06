e469fbbf2f5df422a001d858797fc12f4307bfe1
NAZIK-Dairbekovna <nazikdairbekova2@gmail.com>
2023-11-06 23:57:19 +0600

    first commit

---
+++ leetcode.js
@@ -0,0 +1,16 @@
+// /**
+//  * @return {Function}
+//  */
+let createHelloWorld = function() {
+
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+const a = createHelloWorld();
+
+// /**
+//  * const f = createHelloWorld();
+//  * f(); // "Hello World"
+//  */
