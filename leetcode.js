9d7b852003d61c3c40e57cb29bf8720197e4a37b
NAZIK-Dairbekovna <nazikdairbekova2@gmail.com>
2023-11-08 22:30:51 +0600

    add h2 tag

--- leetcode.js
+++ leetcode.js
@@ -14,3 +14,15 @@ const a = createHelloWorld();
 //  * const f = createHelloWorld();
 //  * f(); // "Hello World"
 //  */
+
+
+// 2620. Counter
+// Given an integer n, return a counter function.
+// This counter function initially returns n and then returns
+// 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+let createCounter = function (n) {
+    let count = n;
+    return function () {
+        return count++;
+    }
+};
