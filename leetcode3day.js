da5804c8d7c9b39906ca3f43618a0e1e0b084c41
NAZIK-Dairbekovna <nazikdairbekova2@gmail.com>
2023-11-16 23:47:42 +0600

    add Two Numbers part 2

--- leetcode3day.js
+++ leetcode3day.js
@@ -1,15 +1,15 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val, next) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.next = (next===undefined ? null : next)
+//  * }
+//  */
+// /**
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+//  */
 let addTwoNumbers = function(l1, l2) {
     let carry = 0;
     let prevNode = new ListNode();
@@ -21,5 +21,18 @@ let addTwoNumbers = function(l1, l2) {
             val1 = l1.val;
             l1 = l1.next;
         }
+        if(l2) {
+            val2 = l2.val
+            l2 = l2.next;
+        }
+
+        const sum = val1 + val2 + carry;
+        carry = Math.floor(sum/10);
+        const digit = sum % 10;
+        const currentNode = new ListNode(digit);
+        prevNode.next = currentNode;
+        prevNode = currentNode;
     }
+
+    return headNode.next;
 };
