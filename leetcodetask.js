8ddc1698f42da0654003f1ebc47cf34f61c8f5e9
NAZIK-Dairbekovna <nazikdairbekova2@gmail.com>
2023-11-25 21:49:29 +0600

    Regular Expression Matching

--- leetcodetask.js
+++ leetcodetask.js
@@ -62,3 +62,36 @@ var isPalindrome = function(x) {
     }
     return x === reversed;
 };
+
+// 10. Regular Expression Matching
+// /**
+//  * @param {string} s
+//  * @param {string} p
+//  * @return {boolean}
+//  */
+var isMatch = function(s, p) {
+    const check = (s, p, i, j) => {
+        if (j > p.length - 1) {
+            return i > s.length - 1
+        }
+
+        const isNextQuantiyModifier = p[j + 1] === '*'
+
+        if (!isNextQuantiyModifier) {
+            if ([s[i], '.'].includes(p[j])) {
+                return i < s.length && check(s, p, i + 1, j + 1)
+            } else {
+                return false
+            }
+        } else {
+            if ([s[i], '.'].includes(p[j])) {
+                return check(s, p, i, j + 2) || (i < s.length && check(s, p, i + 1, j))
+            } else {
+                return check(s, p, i, j + 2)
+            }
+        }
+
+    }
+
+    return check(s, p, 0, 0)
+};
