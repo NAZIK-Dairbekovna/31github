1accf28b333a541b62788ce12f71208d4efb8f65
NAZIK-Dairbekovna <nazikdairbekova2@gmail.com>
2023-12-04 23:38:08 +0600

    3sum closest

--- leetcode15_21tasks.js
+++ leetcode15_21tasks.js
@@ -29,3 +29,29 @@ var threeSum = function(nums) {
 
     return result;
 };
+
+// 16. 3sum closest -Medium
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number}
+//  */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b);
+    let ans = nums[0]+nums[1]+nums[2];
+    for (let i = 0; i < nums.length-2; i+=1) {
+        let start = i + 1, end = nums.length-1;
+        while (start < end) {
+            const sum = nums[i]+nums[start]+nums[end];
+            if (sum > target) {
+                end -= 1;
+            } else {
+                start += 1;
+            }
+            if (Math.abs(target-sum) < Math.abs(target-ans)) {
+                ans = sum;
+            }
+        }
+    }
+    return ans;
+};
