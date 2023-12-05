620c65d6865e4ef6f4520c4b7325d9c88afbaef4
NAZIK-Dairbekovna <nazikdairbekova2@gmail.com>
2023-12-05 22:06:50 +0600

    letter combinations of a phone number

--- leetcode15_21tasks.js
+++ leetcode15_21tasks.js
@@ -55,3 +55,26 @@ var threeSumClosest = function(nums, target) {
     }
     return ans;
 };
+
+// 17. letter combinations of a phone number -Medium
+// /**
+//  * @param {string} digits
+//  * @return {string[]}
+//  */
+var letterCombinations = function(digits) {
+    if (digits.length === 0) return [];
+    const map = {
+        '2': 'abc', '3': 'def', '4': 'ghi',
+        '5': 'jkl', '6': 'mno', '7': 'pqrs',
+        '8': 'tuv', '9': 'wxyz'
+    };
+    const queue = [''];
+    for (const digit of digits) {
+        const len = queue.length;
+        for (let i=0; i<len; i++) {
+            const current = queue.shift()
+            map[digit].split('').forEach(i => queue.push(current + i));
+        }
+    }
+    return queue;
+};
